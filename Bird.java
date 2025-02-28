c1631284f0c3eac2b838057d4a905aa63f06eef6
Giuliano <gproietti07@icloud.com>
2025-02-28 09:54:30 -0500

    Initial sharing of project

---
+++ Bird.java
@@ -0,0 +1,28 @@
+
+
+public class Bird
+{
+    // instance variables - replace the example below with your own
+    private String color;
+    private String breed;
+    private int age;
+
+    /**
+     * Constructor for objects of class Bird
+     */
+    public Bird(String color, String breed, int age)
+    {
+        // initialise instance variables
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+    
+
+    
+    public void sing()
+    {
+        // put your code here
+        System.out.println("Brid vocalization in action...");
+    }
+}
---
+++ Salary.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Salary here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salary
+{
+    public int salary(){
+    hourlyWage * hours = salary;
+    int taxes = 0.3;
+    int deductions = salary * taxes;
+    int netPay = salary - deductions;
+    System.out.println("The net pay is:" + netPay);
+    }
+}
